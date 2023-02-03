cdc5223252a4f86e683df64eeaa95e59ee96ca35
Aleksey <meysonsib200189@mail.ru>
2023-02-03 18:04:07 +0700

    class MainViewModel(application : Application) : AndroidViewModel(application) передача контекста во ViewModel

--- app/src/main/java/com/hfad/myappviewmodel/MainViewModel.kt
+++ app/src/main/java/com/hfad/myappviewmodel/MainViewModel.kt
@@ -1,24 +1,26 @@
 package com.hfad.myappviewmodel
 
+import android.app.Application
 import android.os.CountDownTimer
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application : Application) : AndroidViewModel(application) {
 
     val liveData = MutableLiveData<String>()
 
     init {
         startTimer()
     }
-
-
     fun startTimer(){
         object : CountDownTimer(15000,1000){
             override fun onTick(p0: Long) {
                liveData.value = ((p0/1000).toString())
             }
             override fun onFinish() {
+                Toast.makeText(getApplication(),"Тут применена AndroidViewModel у которой есть контекст",Toast.LENGTH_LONG).show()
             }
         }.start()
     }
